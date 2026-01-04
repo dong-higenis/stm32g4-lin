b860a598b61843a3596639f969ada1210d18bfdb
dong-higenis <dong@higenis.com>
2026-01-04 20:55:33 +0900

    STM32CubeIDE 1.18.0 으로 이주 디버그 메시지 추가 보레이드 설정 안 바뀌는 문제 수정 UART PIN Swap 원상 복구 LIN EN핀 제어

--- source/stm32g4_lin/Debug/open-LIN-c/open_lin_transport_layer.d
+++
@@ -1,8 +0,0 @@
-open-LIN-c/open_lin_transport_layer.o: \
- ../open-LIN-c/open_lin_transport_layer.c \
- ../open-LIN-c/open_lin_transport_layer.h ../open-LIN-c/open_lin_types.h \
- ../open-LIN-c/open_lin_cfg.h ../open-LIN-c/open_lin_network_layer.h
-../open-LIN-c/open_lin_transport_layer.h:
-../open-LIN-c/open_lin_types.h:
-../open-LIN-c/open_lin_cfg.h:
-../open-LIN-c/open_lin_network_layer.h:
